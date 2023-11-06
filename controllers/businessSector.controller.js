186f16611e8f827fe2656bb692030863112390e1
Trangna <trangna.ftu@gmail.com>
2023-11-06 23:44:21 +0700

    N3-34

--- controllers/businessSector.controller.js
+++ controllers/businessSector.controller.js
@@ -2,8 +2,7 @@ import asyncHandler from "express-async-handler";
 import { businessSectorGetAll } from "../services/mongo/businessSectors.js";
 import { RESPONSE } from "../globals/api.js";
 import { ResponseFields } from "../globals/fields/response.js";
-
-
+import { createBusinessSector, updateBusinessSector } from "../services/mongo/businessSectors.js";
 
 const getAll = asyncHandler(async (req, res) => {
   try {
@@ -21,12 +20,32 @@ const getAll = asyncHandler(async (req, res) => {
   }
 });
 
+const createBSector = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const createdBusinessSector = await createBusinessSector(name);
+    res.status(201).json(createdBusinessSector);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
 
-
-
+// Update an existing business sector by ID
+const updateBSector = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const businessSectorId = req.params.id;
+    const updatedBusinessSector = await updateBusinessSector(businessSectorId, name);
+    res.status(200).json(updatedBusinessSector);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
 
 const BusinessSectorController = {
-  getAll
+  getAll,
+  createBSector,
+  updateBSector
 };
 
 export default BusinessSectorController;
--- routes/businessSector.route.js
+++ routes/businessSector.route.js
@@ -1,13 +1,14 @@
 import express from "express";
 import BusinessSectorController from "../controllers/businessSector.controller.js";
-
+import { jwtCheck } from "../middlewares/jwt.js";
+import { validationProfileMdw } from "../middlewares/validate.middleware.js";
+import checkRole from "../middlewares/checkRole.middleware.js";
 
 const bSRouter = express.Router();
 
-
-
+bSRouter.use(jwtCheck, validationProfileMdw);
 bSRouter.get("/", BusinessSectorController.getAll);
-
-
+bSRouter.post("/create", checkRole(["admin", "superadmin"]), BusinessSectorController.createBSector);
+bSRouter.put("/update", checkRole(["admin", "superadmin"]), BusinessSectorController.updateBSector)
 
 export default bSRouter;
